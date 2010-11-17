0cbdccae07ede0eb8ecbe2094ce40c073c474ebb
unknown <srikann@.corporate.thoughtworks.com>
2010-11-17 12:41:35 -0600

    Created a home screen with a Editview,Button and TextView need to link them to get the desired stock quote

--- src/com/tw/Home.java
+++ src/com/tw/Home.java
@@ -8,6 +8,6 @@ public class Home extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.home_page);
     }
 }
