454fa9ee278d510e509f557b3fcf37c1c14becb3
Srikanth Nutigattu <srikanth.nutigattu@thoughtworks.com>
2010-11-18 12:47:52 -0600

    Dynamically fetching quote for the qiven symbol

--- src/com/tw/activities/Home.java
+++ src/com/tw/activities/Home.java
@@ -7,21 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.tw.activities.R;
 import com.tw.services.DataRetrieverService;
 
 public class Home extends Activity {
-    /** Called when the activity is first created. */
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-
+        
+        
         Button getQuote = (Button)findViewById(R.id.find_quote);        
         getQuote.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				new UpdateQuoteTask().execute("some url");
+				new UpdateQuoteTask().execute(((EditText)findViewById(R.id.stock_symbol)).getText().toString());
 			}
 		});    
     }
@@ -37,7 +40,7 @@ public class Home extends Activity {
 
 		@Override
 		protected String doInBackground(String... params) {
-			return (new DataRetrieverService().getQuote("some symbol"));
+			return (new DataRetrieverService().getQuote(params[0]));
 		}
     }
         
--- src/com/tw/services/DataRetrieverService.java
+++ src/com/tw/services/DataRetrieverService.java
@@ -13,7 +13,8 @@ import org.apache.http.util.EntityUtils;
 public class DataRetrieverService {
 
 	public String getQuote(String symbol) {
-		HttpRequest request = new HttpGet("http://hk.finance.yahoo.com/d/quote.csv?s=0005.HK&f=sl1d1t1c1ohgv");
+		//HttpRequest request = new HttpGet("http://finance.yahoo.com/d/quote.csv?s=SATYAMCOM.NS&f=sl1d1t1c1ohgv");
+		HttpRequest request = new HttpGet("http://finance.yahoo.com/d/quote.csv?s="+symbol+"&f=sl1d1t1c1ohgv");
         HttpClient client = new DefaultHttpClient();  
 		try {
 			 HttpResponse response = client.execute((HttpUriRequest) request);
