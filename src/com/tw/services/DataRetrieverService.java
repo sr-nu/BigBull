a56358639ab9bd7e55f576318f65d12ba8ca88e3
Srikanth Nutigattu <srikanth.nutigattu@thoughtworks.com>
2010-12-01 14:02:00 -0600

    Added a screen to demonstrate autofill activity where a country name is suggested as the user types in the text box. Try to adapt the same thing for retrieving company names.

--- src/com/tw/services/DataRetrieverService.java
+++ src/com/tw/services/DataRetrieverService.java
@@ -1,6 +1,8 @@
 package com.tw.services;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -9,9 +11,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class DataRetrieverService {
+	
+	Map<String,String> companyNameSymbolMap = new TreeMap<String,String>();
 	
 	
 	public String getQuote(String symbol) {
@@ -35,6 +42,28 @@ public class DataRetrieverService {
 		return "N/A";
 	}
 
+	public String[] getCompanyNames(String companyName) {
+		
+		String response = sendHttpRequest("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="+companyName+"&callback=YAHOO.Finance.SymbolSuggest.ssCallback");
+		if(response != null && (!response.trim().equals("")) &&response.contains("\"Result\":")) {
+			JSONTokener jsonTokener = new JSONTokener(response.substring(response.indexOf("(")+1,response.lastIndexOf(")")));
+			try {
+				JSONArray nextValue = ((JSONObject) ((JSONObject) jsonTokener.nextValue()).get("ResultSet")).getJSONArray("Result");
+				
+				
+				for(int i=0; i<nextValue.length(); i++) {
+					companyNameSymbolMap.put(((JSONObject)nextValue.get(i)).getString("company"),"symbol");
+				}
+				
+				return companyNameSymbolMap.keySet().toArray(new String[0]);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return new String[]{};
+	}
+	
+	
 	private String sendHttpRequest(String URL) {
 		HttpRequest request = new HttpGet(URL);
         HttpClient client = new DefaultHttpClient();  
